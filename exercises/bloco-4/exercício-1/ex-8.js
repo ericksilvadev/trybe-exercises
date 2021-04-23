86c3a20da08831c05223fadb5c8f2eea13dea625
Erick Silva <erick.silva6653@gmail.com>
2021-04-22 21:31:38 -0300

    exercicio 9 completo

--- exercises/bloco-4/exercício-1/ex-8.js
+++ exercises/bloco-4/exercício-1/ex-8.js
@@ -1,8 +1,8 @@
-let a = 6;
+let a = 7;
 let b = 13;
-let c = 20;
+let c = 21;
 
-if(a % 2 == 0 && b % 2 == 0 && c % 2 == 0){
+if(a % 2 == 1 && b % 2 == 1 && c % 2 == 1){
     console.log(false);
 } else {
     console.log(true);
---
+++ exercises/bloco-4/exercício-1/ex-9.js
@@ -0,0 +1,9 @@
+let a = 6;
+let b = 12;
+let c = 20;
+
+if(a % 2 == 0 && b % 2 == 0 && c % 2 == 0){
+    console.log(false);
+} else {
+    console.log(true);
+}
