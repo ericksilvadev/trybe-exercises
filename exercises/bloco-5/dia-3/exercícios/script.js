4e0fddf49d3767dad6e5b27db037981a947e8a31
Erick Silva <erick.silva6653@gmail.com>
2021-05-03 23:27:05 -0300

    exercicios do 4 ao 6

--- exercises/bloco-5/dia-3/exercícios/script.js
+++ exercises/bloco-5/dia-3/exercícios/script.js
@@ -18,22 +18,21 @@ createDaysOfTheWeek();
 // 1
 
 function getDays() {
-  const daysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+  const daysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   let days = document.getElementById('days');
   for (index = 0; index < daysList.length; index += 1) {
     let day = document.createElement('li');
     day.innerHTML = daysList[index];
-    day.classList.add('day')
+    day.classList.add('day');
     days.appendChild(day);
     if (daysList[index] === 24 || daysList[index] === 31) {
-      day.classList.add('holiday')
+      day.classList.add('holiday');
     } else if (daysList[index] === 4 || daysList[index] === 11 || daysList[index] === 18) {
-      day.classList.add('friday')
+      day.classList.add('friday');
     } else if (daysList[index] === 25) {
-      day.classList.add('friday', 'holiday')
+      day.classList.add('friday', 'holiday');
     }
   }
-  console.log(days);
 }
 
 getDays()
@@ -42,28 +41,27 @@ getDays()
 
 function holidaysBtn() {
   let btnHoliday = document.createElement('button');
-  btnHoliday.id = 'btn-holiday'
-  btnHoliday.innerHTML = 'Feriados'
+  btnHoliday.id = 'btn-holiday';
+  btnHoliday.innerHTML = 'Feriados';
   let btnContainer = document.getElementsByClassName('buttons-container')[0];
   btnContainer.appendChild(btnHoliday);
-  console.log(btnContainer);
 }
 
 holidaysBtn();
 
 // 3
 
-let btnHoliday = document.getElementById('btn-holiday')
+let btnHoliday = document.getElementById('btn-holiday');
 
 btnHoliday.addEventListener('click', function () {
   holidays = document.querySelectorAll('.holiday');
   for (let i = 0; i < holidays.length; i += 1) {
-    if (holidays[i].classList.contains('active')) {
-      holidays[i].classList.remove('active')
+    if (holidays[i].classList.contains('active-holiday')) {
+      holidays[i].classList.remove('active-holiday');
       holidays[i].style.backgroundColor = '#eee';
       holidays[i].style.color = '#777';
     } else {
-      holidays[i].classList.add('active')
+      holidays[i].classList.add('active-holiday');
       holidays[i].style.backgroundColor = '#2fc18c';
       holidays[i].style.color = '#eee';
     }  
@@ -71,4 +69,71 @@ btnHoliday.addEventListener('click', function () {
 });
 
 // 4
+
+function fridayBtn() {
+  let btnFriday = document.createElement('button');
+  btnFriday.id = 'btn-friday';
+  btnFriday.innerHTML = 'Sexta-feira';
+  let btnContainer = document.getElementsByClassName('buttons-container')[0];
+  btnContainer.appendChild(btnFriday);
+}
+
+fridayBtn();
+
+// 5 
+  
+let btnFriday = document.getElementById('btn-friday')
+
+btnFriday.addEventListener('click', function () {
+  fridays = document.querySelectorAll('.friday');
+  for (let i = 0; i < fridays.length; i += 1) {
+    if (fridays[i].classList.contains('active-friday')) {
+      fridays[i].classList.remove('active-friday');
+      daysF = [4, 11, 18, 25];
+      fridays[i].innerHTML = daysF[i]
+    } else {
+      fridays[i].classList.add('active-friday');
+      fridays[i].innerHTML = 'breja';
+    }  
+  }
+});
+
+// 6
+
+let days = document.getElementById('days')
+days.style.backgroundColor = '#eee'
+
+function zoom(event) {
+  event.target.style.fontSize = '25px'
+  // event.target.style.backgroundColor = '#666'
+  event.target.style.color = 'tomato'
+  event.target.style.cursor = 'default'
+
+}
+
+function zoomOut(event) {
+  event.target.style.fontSize = '20px'
+  // event.target.style.backgroundColor = '#eee'
+  event.target.style.color = '#777'
+}
+days.addEventListener('mouseover', zoom)
+days.addEventListener('mouseout', zoomOut)
+
+console.log(days);
+
+// 7
+
+
+function tasks() {
+  let task = document.getElementById('task-input').value;
+  taskSpan = document.createElement('span');
+  myTasks = document.querySelector('.my-tasks');
+  myTasks.appendChild(taskSpan);
+  taskSpan.innerHTML = task + '</br>';
+  console.log(myTasks, task);
+}
+
+let btnAdd = document.getElementById('btn-add');
+
+btnAdd.addEventListener('click', tasks)
 
